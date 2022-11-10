71bf8983bd0969c703955d3d0c116c82e8bfb39e
Oscar Angel <oscaraag1493@gmail.com>
2022-11-10 11:06:24 -0400

    [test] test apk

--- src/App.jsx
+++ src/App.jsx
@@ -3,39 +3,35 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
- 
-
   const print = () => {
-     // send evento to react-native to print ticket
+    // send evento to react-native to print ticket
 
-     const data = {
+    const data = {
       id: 1,
-      name: "Hola ZeroQ"
-     }
+      name: 'Hola ZeroQ',
+    }
 
-     
-  if (window.ReactNativeWebView) {
-    window.ReactNativeWebView.postMessage(JSON.stringify(data))
-  }
-  if (navigator.userAgent === `electron-webview`) {
-    // Note that the sendMessage function needs to be setup in the preload
-    // script and ties to a corresponding main process method/function
-    // Send a message to the main process
-    // eslint-disable-next-line no-console
-    console.log(`print`)
-    window.api.send(`printTicket`, data)
+    if (window.ReactNativeWebView) {
+      window.ReactNativeWebView.postMessage(JSON.stringify(data))
+    }
+    if (navigator.userAgent === `electron-webview`) {
+      // Note that the sendMessage function needs to be setup in the preload
+      // script and ties to a corresponding main process method/function
+      // Send a message to the main process
+      // eslint-disable-next-line no-console
+      console.log(`print`)
+      window.api.send(`printTicket`, data)
 
-    //  window.Bridge.sendMessage('Message from web view index.js');
-  } else {
-    // eslint-disable-next-line no-console
-    console.log(`fail`)
-  }
+      //  window.Bridge.sendMessage('Message from web view index.js');
+    } else {
+      // eslint-disable-next-line no-console
+      console.log(`fail`)
+    }
   }
 
   return (
-    <div className="App">
-     <button onClick={print}>Imprime Pleaseee 🚀</button>
-       
+    <div className='App'>
+      <button onClick={print}>Imprime Pleaseeee 🚀</button>
     </div>
   )
 }
